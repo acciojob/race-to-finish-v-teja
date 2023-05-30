11d07b3fc45dbee59b62eff69e558c44388029df
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-05-30 19:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 window.promises = [];
 
+// Array of promises with random times between 1 and 5 seconds
+for (let i = 0; i < 5; i++) {
+  const randomTime = Math.floor(Math.random() * 5) + 1; // Random time between 1 and 5 seconds
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise ${i + 1} resolved after ${randomTime} seconds`);
+    }, randomTime * 1000);
+  });
+  window.promises.push(promise);
+}
+
+// Use Promise.any() to wait for the first resolved promise
+Promise.any(window.promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.textContent = result;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+
 // Do not change the code above this
 // add your promises to the array `promises`
